c26e34e7a88e4596c86e9c746e2a011ae9377e92
erloen <elroengaming@gmail.com>
2025-10-14 20:58:03 +0200

    fixed bugs cleaned up stats

--- shooter game/code.js
+++ shooter game/code.js
@@ -1,18 +1,30 @@
-const Border = {"x": 500,"y": 500}
 const canvas = document.querySelector('#gameScreen')
 const ctx = canvas.getContext('2d')
-const HitBoxOfSet = 2
-let dificulty = 1;
+const GAME_CONFIG = {
+    BORDER: { x: canvas.width, y: canvas.height },
+    HITBOX_OFFSET: 2,
+    ENEMY_COLORS: [
+        "#FF0000", "#DC143C", "#FFA500", "#800000", "#FF2400",
+        "#6B8E23", "#A0522D", "#006400", "#708090", "#483C32",
+        "#00FFFF", "#8A2BE2", "#7FFF00", "#FFD700", "#FF69B4",
+        "#F5F5F5", "#000000", "#4B0082", "#008080", "#964B00"
+    ],
+    PLAYER_BASE: {
+        SPEED: 2,
+        HEALTH: { MAX: 20, INVULNERABILITY: 10 },
+        PROJECTILE: { COOLDOWN: 50, SPEED: 6, LIFE: 300, DAMAGE: 2 }
+    },
+    ENEMY_BASE: {
+        SPEED: 2,
+        DAMAGE: 2,
+        HEALTH: { MAX: 10, INVULNERABILITY: 10 },
+        MOVE_CHECK_COOLDOWN: 20
+    }
+};
 const mouse = {
     "x": 0,
     "y": 0,
 }
-const enemieColor = [
-    "#FF0000", "#DC143C", "#FFA500", "#800000", "#FF2400",
-    "#6B8E23", "#A0522D", "#006400", "#708090", "#483C32",
-    "#00FFFF", "#8A2BE2", "#7FFF00", "#FFD700", "#FF69B4",
-    "#F5F5F5", "#000000", "#4B0082", "#008080", "#964B00"
-]
 const keys = {
     "w": false,
     "s": false,
@@ -21,15 +33,14 @@ const keys = {
 }
 
 class Entity {
-    constructor(x,y,w,h,vx,vy,rotx,roty,color,stats = {"damage": 20,"health": {"max": 10,"cur":5,"invunerability":10}},type = "entity",form = "circle") {
+    constructor(config = {"dim": {"height": 10, "width": 10},"pos":{"x": 0,"y":0,"rotX":0,"rotY":0},"v": {"x": 0,"y":0},"color": 'red',"stats": {"speed":2,"damage": 20,"health":{"max": 20,"cur":20,"invunerability":10},"projectyle": {"cooldown":50,"speed":6,"life": 20,"damage":2},},},managerInstance) {
         this.active = true
-        this.dim = {"height": h, "width": w,"hitboxHeight": h + HitBoxOfSet, "hitboxWidth": w + HitBoxOfSet}
-        this.v = {"x": vx, "y": vy}
-        this.loc = {"posX":x,"posY":y,"rotX": rotx,"rotY": roty}
-        this.color = color
-        this.type = type; 
-        this.form = form;
-        this.stats = stats;
+        this.manager = managerInstance
+        this.dim = {"height": config.dim.height, "width": config.dim.width,"hitboxHeight": config.dim.height + GAME_CONFIG.HITBOX_OFFSET, "hitboxWidth": config.dim.width + GAME_CONFIG.HITBOX_OFFSET}
+        this.v = config.v
+        this.pos = config.pos
+        this.color = config.color
+        this.stats = config.stats;
     }
     tickUpdate() {
         if(!this.active) return
@@ -42,24 +53,24 @@ class Entity {
     draw() {
         ctx.fillStyle = this.color
         ctx.beginPath()
-        ctx.arc(this.loc.posX,this.loc.posY,this.dim.width,0,2*Math.PI)
+        ctx.arc(this.pos.x,this.pos.y,this.dim.width,0,2*Math.PI)
         ctx.fill()
     }
     move() {
-        const newPosX = this.loc.posX + this.v.x
-        const newPosY = this.loc.posY + this.v.y
-        if(newPosX - this.dim.hitboxWidth <= 0 || newPosX + this.dim.hitboxWidth >= Border.x) {
+        const newPosX = this.pos.x + this.v.x
+        const newPosY = this.pos.y + this.v.y
+        if(newPosX - this.dim.hitboxWidth <= 0 || newPosX + this.dim.hitboxWidth >= GAME_CONFIG.BORDER.x) {
             this.v.x *= -1
         }
-        if(newPosY - this.dim.hitboxWidth <= 0 || newPosY + this.dim.hitboxWidth >= Border.y) {
+        if(newPosY - this.dim.hitboxWidth <= 0 || newPosY + this.dim.hitboxWidth >= GAME_CONFIG.BORDER.y) {
             this.v.y *= -1
         }
-        this.loc.posX += this.v.x
-        this.loc.posY += this.v.y
+        this.pos.x += this.v.x
+        this.pos.y += this.v.y
     }
     checkColission(Object2) {
-        const dx = Object2.loc.posX - this.loc.posX
-        const dy = Object2.loc.posY - this.loc.posY
+        const dx = Object2.pos.x - this.pos.x
+        const dy = Object2.pos.y - this.pos.y
         const d = Math.sqrt(Math.pow(dx,2) + Math.pow(dy,2))
         if(d <= Object2.dim.hitboxWidth + this.dim.hitboxWidth) {
             this.collided()
@@ -74,24 +85,18 @@ class Entity {
     }
 }
 class Player extends Entity{
-    constructor(x, y, w, h, vx, vy, rotx, roty, color,stats = {"speed":2,"damage": 20,"health":{"max": 20,"cur":20,"invunerability":10},"projectyle": {"cooldown":50,"speed":6,"life": 20,"damage":2},}, type ="player", form = 'circle') {
-        super(x, y, w, h, vx, vy, rotx, roty, color, type, form); 
-        this.stats = stats
-        this.type = type; 
-        this.form = form;
-        this.invunerable = stats.health.invunerability;
+    constructor(config = {"dim": {"height": 10, "width": 10},"pos":{"x": 0,"y":0,"rotX":0,"rotY":0},"v": {"x": 0,"y":0},"color": 'red',"stats": {"speed":2,"damage": 20,"health":{"max": 20,"cur":20,"invunerability":10},"projectyle": {"cooldown":50,"speed":6,"life": 20,"damage":2},},},managerInstance) {
+        super(config, managerInstance);
+        this.invunerable = this.stats.health.invunerability;
         this.projectylCooldown = this.stats.projectyle.cooldown
 
     }
-    drawAimHelper() {
-
-    }
     shootProjectyles() {
-        const dx = mouse.x - this.loc.posX
-        const dy = mouse.y - this.loc.posY
+        const dx = mouse.x - this.pos.x
+        const dy = mouse.y - this.pos.y
         const radians = Math.atan2(dy, dx)
-        const x = this.loc.posX + Math.cos(radians) * 40
-        const y = this.loc.posY + Math.sin(radians) * 40
+        const x = this.pos.x + Math.cos(radians) * 40
+        const y = this.pos.y + Math.sin(radians) * 40
         ctx.fillStyle = 'rgba(156, 9, 46, 0.5)'
         ctx.beginPath()
         ctx.arc(x, y,15,0,2*Math.PI)
@@ -100,12 +105,29 @@ class Player extends Entity{
             this.projectylCooldown = this.stats.projectyle.cooldown
             const vx = Math.cos(radians) * this.stats.projectyle.speed
             const vy = Math.sin(radians) * this.stats.projectyle.speed
-            Projectyles.push(new Projectyle(x,y,6,6,vx,vy,0,0,'black',{"damage": 2,"life": 300,"penetrate": 1}))
+            this.manager.Projectyles.push(new Projectyle({
+                "dim": {
+                    "height": 6, 
+                    "width": 6},
+                "pos":{
+                    "x": x,
+                    "y":y,
+                    "rotX":0,
+                    "rotY":0},
+                "v": {
+                    "x": vx,
+                    "y":vy,},
+                "color": 'black',
+                "stats": {
+                    "life":this.stats.projectyle.life,
+                    "penetrate":this.stats.projectyle.penetrate,
+                    "damage":this.stats.projectyle.damage},}
+                ,this.manager))
         }
     }
     draw() {
-        const x = this.loc.posX - this.dim.width;
-        const y = this.loc.posY - this.dim.height; 
+        const x = this.pos.x - this.dim.width;
+        const y = this.pos.y - this.dim.height; 
         const width = this.dim.width * 2;
         const height = this.dim.height * 2;
 
@@ -122,7 +144,6 @@ class Player extends Entity{
         }
         this.invunerable -= 1;
         this.projectylCooldown -= 1;
-        this.drawAimHelper()
         this.handelKeyInput()
         this.checkAllColl()
         super.tickUpdate()
@@ -130,8 +151,8 @@ class Player extends Entity{
         drawText(`Health: ${this.stats.health.cur}/${this.stats.health.max}`,10,20)
     }
     checkAllColl() {
-        for(let entity of Entities) {
-            if(entity.type == "player" || !entity.active) {continue}
+        for(let entity of this.manager.Entities) {
+            if(entity === this || !entity.active) {continue} 
             const collided = this.checkColission(entity)
             if(collided) {
                 this.hitEntity(entity)
@@ -158,11 +179,9 @@ class Player extends Entity{
     
 }
 class Projectyle extends Entity {
-    constructor(x = 0, y = 0, w = 10, h = 10, vx = 0, vy = 0, rotx = 0, roty = 0, color = 'black', stats = {"damage": 20,"life": 300,"penetrate": 1}, type ="Projectyle", form = 'circle') {
-        super(x, y, w, h, vx, vy, rotx, roty, color, type, form); 
-        this.type = type; 
-        this.form = form;
-        this.stats = stats
+    constructor(config = {"dim": {"height": 10, "width": 10},"pos":{"x": 0,"y":0,"rotX":0,"rotY":0},"v": {"x": 0,"y":0},"color": 'red',"stats": {"life":300,"penetrate":1,"damage":2},},managerInstance) {
+        super(config, managerInstance);
+        this.type = "Projectyle"; 
     }
     tickUpdate() {
         this.stats.life -= 1;
@@ -176,7 +195,7 @@ class Projectyle extends Entity {
         super.move()
     }
     checkAllColl() {
-        for(let entity of Entities) {
+        for(let entity of this.manager.Entities) {
             if(!entity.active) {continue}
             const collided = this.checkColission(entity)
             if(collided) {
@@ -193,24 +212,20 @@ class Projectyle extends Entity {
     }
 }
 class Enemy extends Entity {
-    constructor(x, y, w, h, vx, vy, rotx, roty, color,stats = {"speed":2,"moveCheckCooldown": 20,"damage": 2,"health":{"max": 10,"cur":5,"invunerability":10}} , type ="enemy", form = 'circle') {
-        super(x, y, w, h, vx, vy, rotx, roty, color, type, form); 
-        this.speed = 2;
-        this.type = type; 
-        this.form = form;
-        this.stats = stats
-        this.moveCoolown = this.stats.moveCheckCooldown
+    constructor(config = {"dim": {"height": 10, "width": 10},"pos":{"x": 0,"y":0,"rotX":0,"rotY":0},"v": {"x": 0,"y":0},"color": 'red',"stats": {"moveCheckCooldown": 10,"speed":2,"damage": 20,"health":{"max": 20,"cur":20,"invunerability":10},},},managerInstance) {
+        super(config, managerInstance);
+        this.moveCooldown = this.stats.moveCheckCooldown
     }
     tickUpdate() {
-        this.moveCoolown-= 1;
+        this.moveCooldown-= 1;
         super.tickUpdate()
-        drawText(`Enemy: ${this.stats.health.cur}/${this.stats.health.max}`,this.loc.posX- this.dim.hitboxWidth * 2,this.loc.posY - this.dim.hitboxHeight * 2)
+        drawText(`Enemy: ${this.stats.health.cur}/${this.stats.health.max}`,this.pos.x- this.dim.hitboxWidth * 2,this.pos.y- this.dim.hitboxHeight * 2)
     }
     move() {
-        if(this.moveCoolown <= 0) {
-            this.moveCoolown= this.stats.moveCheckCooldown
-            const dx = player.loc.posX - this.loc.posX
-            const dy = player.loc.posY - this.loc.posY
+        if(this.moveCooldown <= 0) {
+            this.moveCooldown= this.stats.moveCheckCooldown
+            const dx = this.manager.player.pos.x - this.pos.x
+            const dy = this.manager.player.pos.y - this.pos.y
             const radians = Math.atan2(dy, dx)
             const vx = Math.cos(radians)  * this.stats.speed
             const vy = Math.sin(radians) * this.stats.speed
@@ -220,75 +235,121 @@ class Enemy extends Entity {
         super.move()
     }
 }
-class WaveManager {
-    constructor(baseV = {"maxHealth": 4, "speed": 2,"damage":2,"enemyCount":1}) {
-        this.baseV = baseV
+class GameManager {
+    constructor() {
         this.Wave = 0
+        this.player = new Player({
+            "dim": {
+                "height": 20, 
+                "width": 20},
+            "pos":{
+                "x": GAME_CONFIG.BORDER.x /2,
+                "y":GAME_CONFIG.BORDER.y/2,
+                "rotX":0,
+                "rotY":0},
+            "v": {
+                "x": 0,
+                "y":0},
+            "color": 'blue',
+            "stats": {
+                "speed":GAME_CONFIG.PLAYER_BASE.SPEED,
+                "damage": 2,
+                "health":{
+                    "max": GAME_CONFIG.PLAYER_BASE.HEALTH.MAX,
+                    "cur":GAME_CONFIG.PLAYER_BASE.HEALTH.MAX,
+                    "invunerability":GAME_CONFIG.PLAYER_BASE.HEALTH.INVULNERABILITY},
+                "projectyle": {
+                    "cooldown":GAME_CONFIG.PLAYER_BASE.PROJECTILE.COOLDOWN,
+                    "speed":GAME_CONFIG.PLAYER_BASE.PROJECTILE.SPEED,
+                    "life": GAME_CONFIG.PLAYER_BASE.PROJECTILE.LIFE,
+                    "damage":GAME_CONFIG.PLAYER_BASE.PROJECTILE.DAMAGE,
+                    "penetrate": 1
+                },},}
+            ,this)
+        this.Projectyles = [
+            
+        ]
+        this.Entities = [
+            
+        ]
+        requestAnimationFrame(this.gameLoop)
     }
     spawnNewWave() {
-        const health = this.baseV.maxHealth + (5 * this.Wave) + (1.5 * Math.pow(this.Wave, 2));
-        const speed = this.baseV.speed + (0.1 * this.Wave) + (0.01 * Math.pow(this.Wave, 2));
-        const damage = Math.floor((this.baseV.damage + (1 * this.Wave) + (0.3 * Math.pow(this.Wave, 2))));
-        const EnemyCount = Math.ceil(this.baseV.enemyCount + (this.Wave / 5) + (Math.pow(this.Wave, 2) / 200));
-        console.log(EnemyCount)
+        const health = Math.floor(GAME_CONFIG.ENEMY_BASE.HEALTH.MAX + 2*Math.cbrt(Math.pow(this.Wave,2.5)));
+        const speed = GAME_CONFIG.ENEMY_BASE.SPEED + (0.1 * this.Wave) + (0.01 * Math.pow(this.Wave, 2));
+        const damage = Math.floor((GAME_CONFIG.ENEMY_BASE.DAMAGE + (1 * this.Wave) + (0.3 * Math.pow(this.Wave, 2))));
+        const EnemyCount = Math.ceil(1 + (this.Wave / 5) + (Math.pow(this.Wave, 2) / 200));
         for(let i = 0; i < EnemyCount; i++) {
-            const x = 200;
-            const y = 200;
+            const x = 100;
+            const y = 100;
             const size = 20;
-            const color = enemieColor[randInt(0,20)]
-            const stats = {
-                "speed":speed,
-                "moveCheckCooldown": 10,
-                "damage": damage,
-                "health":{"max": health,"cur":health,"invunerability":10}
-            }
-            Entities.push(
-                new Enemy(x,y,size,size,0,0,0,0,color,stats)
+            const color = GAME_CONFIG.ENEMY_COLORS[randInt(0,20)]
+            this.Entities.push(
+                new Enemy({
+                    "dim": {
+                        "height": size, 
+                        "width": size},
+                    "pos":{
+                        "x": x,
+                        "y":y,
+                        "rotX":0,
+                        "rotY":0},
+                    "v": {
+                        "x": 0,
+                        "y":0},
+                    "color": color,
+                    "stats": {
+                        "moveCheckCooldown": GAME_CONFIG.ENEMY_BASE.MOVE_CHECK_COOLDOWN,
+                        "speed":speed,
+                        "damage": damage,
+                        "health":{
+                            "max": health,
+                            "cur": health,
+                            "invunerability": GAME_CONFIG.ENEMY_BASE.MOVE_CHECK_COOLDOWN},
+                        },}
+                    ,this)
             )
         }
         this.Wave += 1
     }
-}
-let Projectyles = [
-    
-]
-let Entities = [
-    
-]
-let gameRunning = true
-const waveManager = new WaveManager()
-const player = new Player(200,290,20,20,0,0,0,0,'blue')
-function gameLoop() {
-    if(gameRunning) {
-        if(!player.active) return
-        EntityUpdate()
-    }else {
+    gameLoop = () => {
+        if(gameRunning) {
+            if(!this.player.active) return
+                this.EntityUpdate()
+            }else {
+        }
+        requestAnimationFrame(this.gameLoop)
     }
-    requestAnimationFrame(gameLoop)
+    EntityUpdate() {
+        ctx.clearRect(0,0,GAME_CONFIG.BORDER.x,GAME_CONFIG.BORDER.y)
+        for(let entity of this.Entities) {
+            entity.tickUpdate()
+        }
+        for(let projectyle of this.Projectyles) {
+            projectyle.tickUpdate()
+        }
+        this.Entities = this.Entities.filter(entity => entity.active);
+        this.Projectyles = this.Projectyles.filter(projectyl => projectyl.active);
+        if(this.Entities.length == 0) {
+           this.spawnNewWave()
+        }
+        this.player.tickUpdate()
+    }
 }
+class HomeScreen {
+    constructor() {
+        this.show = true
+    }
+}
+let gameRunning = true
+const waveManager = new GameManager(baseV = {"maxHealth": 4, "speed": 2,"damage":2,"enemyCount":1},playerStats = {"projectyle": {"cooldown":50,"speed":6,"life": 20,"damage":2},"speed":2,"health": 20})
 function gamePaused() {
     ctx.fillStyle = "rgba(255, 0, 179, 0.005)"
-    ctx.fillRect(0,0,Border.x,Border.y)
+    ctx.fillRect(0,0,GAME_CONFIG.BORDER.x,GAME_CONFIG.BORDER.y)
 
-    drawText("Game Paused",Border.x/2,Border.y/2-40, fontColor = 'black', fontSize = '50px', fontFamily = 'sans-serif',textAlign = 'center',textBaseline = 'middle')
-    drawText("click ESC to unPause",Border.x/2,Border.y/2+50, fontColor = 'black', fontSize = '40px', fontFamily = 'sans-serif',textAlign = 'center',textBaseline = 'middle')
+    drawText("Game Paused",GAME_CONFIG.BORDER.x/2,GAME_CONFIG.BORDER.y/2-40, fontColor = 'black', fontSize = '50px', fontFamily = 'sans-serif',textAlign = 'center',textBaseline = 'middle')
+    drawText("click ESC to unPause",GAME_CONFIG.BORDER.x/2,GAME_CONFIG.BORDER.y/2+50, fontColor = 'black', fontSize = '40px', fontFamily = 'sans-serif',textAlign = 'center',textBaseline = 'middle')
 }
-function EntityUpdate() {
-    ctx.clearRect(0,0,Border.x,Border.y)
-    for(let entity of Entities) {
-        entity.tickUpdate()
-    }
-    for(let projectyle of Projectyles) {
-        projectyle.tickUpdate()
-    }
-    Entities = Entities.filter(entity => entity.active);
-    Projectyles = Projectyles.filter(projectyl => projectyl.active);
-    if(Entities.length == 0) {
-        waveManager.spawnNewWave()
-    }
-    player.tickUpdate()
-}
-requestAnimationFrame(gameLoop)
 document.addEventListener('keydown', (e) => {
     const key = e.key 
     if(e.key === 'Escape') {
